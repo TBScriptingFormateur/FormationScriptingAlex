bd824115782433edfb1c65d337b39520ce54a388
TBScriptingFormateur <alarriger@gmail.com>
2025-06-09 14:15:49 +0200

    Create my_script.js
    mon premier fichier

---
+++ my_script.js
@@ -0,0 +1,17 @@
+function my_func() {
+
+  // crtl+ D = dupliquer 
+  // crtl+shift  up or down = drag and drop 
+  // crtl+  left or rigth = sauter une section 
+  // crt+F = find and replace 
+  // tab sur plusieur lignes : deplace tout vers la droite  
+
+  MessageBox.information("bonjouer5")
+
+}
+
+// je change dans toonboom
+
+
+
+
